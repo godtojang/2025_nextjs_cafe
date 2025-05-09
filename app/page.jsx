faae8e61cc2454a7feb151ca9c0c3f7ab649a5ac
GodOuk <godjongouk@naver.com>
2025-05-09 18:55:27 +0900

    Fix: update Slider import in page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
-import Contents from "@/components/home/contents/Contents";
-import Slider from "@/components/home/slider/Slider";
-import Aside from "@/components/home/aside/Aside";
-import MdSlider from "@/components/home/mdSlider/MdSlider";
+import Contents from '@/components/home/contents/Contents';
+import Aside from '@/components/home/aside/Aside';
+import MdSlider from '@/components/home/mdSlider/MdSlider';
+import Slider from '@/components/home/Slider/Slider';
 
 export default function Home() {
   return (
